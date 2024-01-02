bbd08f4ec6dd84103182a9df3278ed25c90b7800
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-01 21:24:54 -0600

    Created: main theme

---
+++ src/theme/Theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    primary: '#38B6FF',
+    grayDark: '#575655',
+    grayLight: '#EBEBEB',
+    black: '#000000',
+    white: '#ffffff',
+    success: '#66A15A',
+    error: '#CF0B0B',
+  },
+  fonts: {
+    poppins: 'Poppins',
+    lato: 'Lato',
+  },
+}
