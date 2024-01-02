913b0fdeea6617b9f14c090ed2e2337be9544c23
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-01-01 21:38:44 -0600

    Added: GlobalStyles

--- src/theme/AppTheme.tsx
+++ src/theme/AppTheme.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './Theme';
+import { GlobalStyles } from './GlobalStyles';
 
 interface Props {
   children: JSX.Element;
@@ -8,6 +9,7 @@ interface Props {
 export const AppTheme: React.FC<Props> = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       {children}
     </ThemeProvider>
   )
