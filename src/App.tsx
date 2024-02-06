b4bb5b40c1809a886d5f015f98adb267b84db327
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-02-05 19:59:23 -0600

    Added: <AuthProvider />

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { AppRouter } from './router';
 import { AppTheme } from './theme/AppTheme';
-import { useEffect, useState } from 'react';
-import { supabase } from './supabase';
-import { Session } from '@supabase/supabase-js';
+import { AuthProvider } from './auth/context';
 
 const router = createBrowserRouter(AppRouter);
 
 export const App = () => {
-  const [session, setSession] = useState<Session | null>(null);
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      console.log(session);
-      setSession(session);
-    })
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      console.log({ session, _event });
-      setSession(session);
-    })
-
-    return () => subscription.unsubscribe()
-  }, [])
   return (
     <AppTheme>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </AppTheme>
   )
 }
