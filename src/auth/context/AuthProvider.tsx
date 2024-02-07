959d01bd0a868fa58f80ca8552deb616b8647592
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-02-07 14:00:45 -0600

    Created: onAuthStateChange functionality

--- src/auth/context/AuthProvider.tsx
+++ src/auth/context/AuthProvider.tsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from 'react';
 import { AuthContext } from './';
+import { Session } from '@supabase/supabase-js';
+import { supabase } from '../../supabase';
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const AuthProvider = ({ children }: Props) => {
+  const [session, setSession] = useState<Session | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setIsLoading(false);
+      if(session) {
+        setSession(session);
+      }
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
   return (
     <AuthContext.Provider value={{
-
+      session,
+      isLoading,
     }}>
       {children}
     </AuthContext.Provider>
