eaf5479f3278853836d9afa2efa329596c024f32
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-02-08 23:17:18 -0600

    Created: setSession method

--- src/stores/auth/auth.store.ts
+++ src/stores/auth/auth.store.ts
@@ -2,36 +2,49 @@ import { StateCreator, create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { supabase } from '../../supabase';
 import type { User } from '../../interfaces';
+import { Session } from '@supabase/supabase-js';
 
 export interface AuthState {
-  isAuth: boolean;
+  statusSession: AuthStatus;
   user?: User;
 
-  setUser: (user: User) => void;
+  setSession: (session: Session | null) => void;
   signInWithGoogle: () => Promise<void>;
   signOut: () => Promise<void>;
 }
 
+export type AuthStatus = 'authorized' | 'unauthorized' | 'pending';
 
 const storeApi: StateCreator<AuthState> = (set) => ({
-  isAuth: false,
+  statusSession: 'pending',
   user: undefined,
 
-  setUser: (user: User) => set({ user }),
+  setSession: (session: Session | null) => {
+    if (session) {
+      set({
+        statusSession: 'authorized',
+        user: {
+          id: session.user.id,
+          email: session.user.user_metadata.email,
+          fullName: session.user.user_metadata.full_name,
+        }
+      })
+    } else {
+      set({ statusSession: 'unauthorized' })
+    }
+  },
 
   signInWithGoogle: async () => {
     try {
       await supabase.auth.signInWithOAuth({ provider: 'google' });
-      set({ isAuth: true });
     } catch (error) {
-      set({ isAuth: false });
-      throw new Error('Ocurrió un error en la autenticación.');
+      throw new Error('Ocurrió un error en la autenticación con Google.');
     }
   },
   signOut: async () => {
     try {
       await supabase.auth.signOut();
-      set({ isAuth: false });
+      set({ statusSession: 'unauthorized', user: undefined });
     } catch (error) {
       throw new Error('Ocurrió un error durante el cierre de sesión.');
     }
