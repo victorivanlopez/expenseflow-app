b3012c1b0cf1aef768d827a27fff060015598628
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-02-08 23:18:09 -0600

    Created: onAuthStateChange functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { AppRouter } from './router';
 import { AppTheme } from './theme/AppTheme';
-import { AuthProvider } from './auth/context';
+import { useEffect } from 'react';
+import { supabase } from './supabase';
+import { useAuthStore } from './stores';
 
 const router = createBrowserRouter(AppRouter);
 
 export const App = () => {
+  const setSession = useAuthStore(state => state.setSession);
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe()
+  }, []);
 
   return (
     <AppTheme>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <RouterProvider router={router} />
     </AppTheme>
   )
 }
