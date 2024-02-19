d7c30f250b4ed8a1cc4bd767391441a5b08a4cb1
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-02-19 16:23:55 -0600

    Added: conditions views

--- src/auth/pages/ResetPasswordPage.tsx
+++ src/auth/pages/ResetPasswordPage.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
+import { useAuthStore } from '../../stores';
+import { Alert } from '../../ui/components';
 import { ResetPasswordForm } from '../components/forms/ResetPasswordForm';
 import { ContainerForm, HeaderForm } from './styles';
+import { AlertResponse } from '../../interfaces';
 
 export const ResetPasswordPage = () => {
+  const isChangingPass = useAuthStore(state => state.isChangingPass);
+  const [alert, setAlert] = useState<AlertResponse>({
+    message: '',
+  });
+
+  useEffect(() => {
+    if (!isChangingPass) {
+      return setAlert({
+        message: 'Acción no válida.',
+        type: 'error'
+      });
+    }
+  }, [isChangingPass])
+
+
   return (
     <ContainerForm>
       <HeaderForm>
@@ -9,7 +28,9 @@ export const ResetPasswordPage = () => {
         <p>Indicanos tu nueva contraseña de acceso</p>
       </HeaderForm>
 
-      <ResetPasswordForm />
+      {isChangingPass && <ResetPasswordForm />}
+
+      {!isChangingPass && <Alert alert={alert} />}
 
     </ContainerForm>
   )
