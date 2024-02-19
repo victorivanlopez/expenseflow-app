035022b48dc1e51c693fb49776a24f3d0cee0207
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-02-19 12:03:03 -0600

    Updated: onAuthStateChange functionality

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,19 @@ const router = createBrowserRouter(AppRouter);
 
 export const App = () => {
   const setSession = useAuthStore(state => state.setSession);
+  const setIsChangingPass = useAuthStore(state => state.setIsChangingPass);
 
   useEffect(() => {
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
+      console.log({ _event, session })
+      if (_event === 'INITIAL_SESSION') {
+        setSession(session);
+      } else if (_event === 'PASSWORD_RECOVERY') {
+        setSession(null);
+        setIsChangingPass(true);
+      }
     });
 
     return () => subscription.unsubscribe()
