4b6cd0e1dc2ffccc0410054b2a562b912ce1b09e
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-02-19 12:06:49 -0600

    Created: ForgotPassForm functionality

--- src/auth/components/forms/ForgotPassForm.tsx
+++ src/auth/components/forms/ForgotPassForm.tsx
@@ -1,25 +1,63 @@
-import { 
-  ButtonForm, 
-  FieldForm, 
-  Form, 
-  InputForm 
+import { FormEvent, useState } from 'react';
+import {
+  ButtonForm,
+  FieldForm,
+  Form,
+  InputForm
 } from './styles';
+import { useAuthStore } from '../../../stores';
+import { Alert } from '../../../ui/components';
+import type { AlertResponse } from '../../../interfaces';
 
 export const ForgotPassForm = () => {
-  return (
-    <Form>
-      <FieldForm>
-        <InputForm
-          type="email"
-          placeholder="Email"
-        />
-      </FieldForm>
 
-      <ButtonForm
-        type="submit"
-      >
-        Continuar
-      </ButtonForm>
-    </Form>
+  const sendEmailPasswordReset = useAuthStore(state => state.sendEmailPasswordReset);
+
+  const [email, setEmail] = useState('');
+  const [alert, setAlert] = useState<AlertResponse>({
+    message: '',
+  });
+
+  const onSubmitForm = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (email === '') {
+      return setAlert({
+        message: 'El correo es obligatorio.',
+        type: 'error'
+      });
+    }
+
+    const response = await sendEmailPasswordReset(email);
+
+    if (response.type === 'error') {
+      return setAlert(response);
+    }
+
+    setAlert(response);
+  }
+
+  return (
+    <>
+      {alert.message && <Alert alert={alert} />}
+
+      <Form onSubmit={onSubmitForm}>
+        <FieldForm>
+          <InputForm
+            type="email"
+            placeholder="Email"
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FieldForm>
+
+        <ButtonForm
+          type="submit"
+        >
+          Continuar
+        </ButtonForm>
+      </Form>
+    </>
   )
 }
