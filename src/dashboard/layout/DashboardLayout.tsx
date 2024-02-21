5c30a2b58e804926c26401f4444520940237ab31
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-02-21 15:06:55 -0600

    Added: <Header />

--- src/dashboard/layout/DashboardLayout.tsx
+++ src/dashboard/layout/DashboardLayout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import { Header } from '../components'
 
 export const DashboardLayout = () => {
   return (
     <>
-      <h1>DashboardLayout</h1>
+      <Header />
       <Outlet />
     </>
   )
