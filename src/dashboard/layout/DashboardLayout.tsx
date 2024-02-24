e085c6fd1b92b52834786ba9c9edc6cdb5f5cc98
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-02-23 21:02:00 -0600

    Added: showUserMenu functionality

--- src/dashboard/layout/DashboardLayout.tsx
+++ src/dashboard/layout/DashboardLayout.tsx
@@ -1,11 +1,26 @@
-import { Outlet } from 'react-router-dom'
-import { Header } from '../components'
+import { Outlet } from 'react-router-dom';
+import { Header } from '../components';
+import { MainContainer } from './styles';
+import { useState } from 'react';
 
 export const DashboardLayout = () => {
+  const [showUserMenu, setShowUserMenu] = useState<boolean>(false);
+
+  const onClickContainer = () => {
+    if(showUserMenu) {
+      setShowUserMenu(!showUserMenu);
+    }
+  }
+
   return (
-    <>
-      <Header />
+    <MainContainer
+      onClick={onClickContainer}
+    >
+      <Header
+        showUserMenu={showUserMenu}
+        setShowUserMenu={setShowUserMenu}
+      />
       <Outlet />
-    </>
+    </MainContainer>
   )
 }
