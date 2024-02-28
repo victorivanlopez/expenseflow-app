71f33fdebe78c6b2f42eee1eea49a6aa5288c509
Víctor Iván López González <victor_ivan2211@hotmail.com>
2024-02-28 13:13:08 -0600

    Added: signOut functionality

--- src/dashboard/components/sidebar/index.tsx
+++ src/dashboard/components/sidebar/index.tsx
@@ -1,13 +1,20 @@
-import { NavLink } from 'react-router-dom';
-import { NavigationSeparator, NavigationSidebar, SidebarContainer } from './styles';
+import { useAuthStore } from '../../../stores';
+import {
+  NavigationLink,
+  NavigationSeparator,
+  NavigationSidebar,
+  SidebarContainer
+} from './styles';
 
 export const Sidebar = () => {
+  const signOut = useAuthStore(state => state.signOut);
+
   return (
     <SidebarContainer>
       <NavigationSidebar>
         <ul>
           <li>
-            <NavLink
+            <NavigationLink
               className={({ isActive }) => `${isActive ? 'active-nav' : ''}`}
               to="/"
             >
@@ -15,10 +22,10 @@ export const Sidebar = () => {
                 <path fillRule="evenodd" d="M3 6a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3V6ZM3 15.75a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-2.25Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3v-2.25Z" clipRule="evenodd" />
               </svg>
               <span>Dashboard</span>
-            </NavLink>
+            </NavigationLink>
           </li>
           <li>
-            <NavLink
+            <NavigationLink
               className={({ isActive }) => `${isActive ? 'active-nav' : ''}`}
               to="/expenses"
             >
@@ -26,13 +33,13 @@ export const Sidebar = () => {
                 <path fillRule="evenodd" d="M15.97 2.47a.75.75 0 0 1 1.06 0l4.5 4.5a.75.75 0 0 1 0 1.06l-4.5 4.5a.75.75 0 1 1-1.06-1.06l3.22-3.22H7.5a.75.75 0 0 1 0-1.5h11.69l-3.22-3.22a.75.75 0 0 1 0-1.06Zm-7.94 9a.75.75 0 0 1 0 1.06l-3.22 3.22H16.5a.75.75 0 0 1 0 1.5H4.81l3.22 3.22a.75.75 0 1 1-1.06 1.06l-4.5-4.5a.75.75 0 0 1 0-1.06l4.5-4.5a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
               </svg>
               <span>Movimientos</span>
-            </NavLink>
+            </NavigationLink>
           </li>
           <li>
             <NavigationSeparator></NavigationSeparator>
           </li>
           <li>
-            <NavLink
+            <NavigationLink
               className={({ isActive }) => `${isActive ? 'active-nav' : ''}`}
               to="/settings"
             >
@@ -40,7 +47,18 @@ export const Sidebar = () => {
                 <path fillRule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z" clipRule="evenodd" />
               </svg>
               <span>Configuración</span>
-            </NavLink>
+            </NavigationLink>
+          </li>
+          <li>
+            <button
+              type='button'
+              onClick={signOut}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path fillRule="evenodd" d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z" clipRule="evenodd" />
+              </svg>
+              <span>Cerrar sesión</span>
+            </button>
           </li>
         </ul>
       </NavigationSidebar>
